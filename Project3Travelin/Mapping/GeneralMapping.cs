a63a79683034e2e04870aa9b65cac5697877be35
GulKaratas <glsrn_7872@outlook.com>
2026-02-23 18:45:02 +0300

    MongoDb Add

--- Project3Travelin/Mapping/GeneralMapping.cs
+++ Project3Travelin/Mapping/GeneralMapping.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using Project3Travelin.Dtos.Category;
+using Project3Travelin.Dtos.CategoryDtos;
+using Project3Travelin.Dtos.TourDtos;
 using Project3Travelin.Entities;
 
 namespace Project3Travelin.Mapping
@@ -10,7 +12,14 @@ namespace Project3Travelin.Mapping
         public GeneralMapping()
         {
             CreateMap<Category,CreateCategoryDto>().ReverseMap();
-          
+            CreateMap<Category,ResultCategoryDto>().ReverseMap();
+             CreateMap<Category,GetCategoryByIdDto>().ReverseMap();
+             CreateMap<Category,UpdateCategoryDto>().ReverseMap();  
+
+              CreateMap<Tour,CreateTourDto>().ReverseMap();
+                CreateMap<Tour,GetTourByIdDto>().ReverseMap();
+                    CreateMap<Tour,ResultTourDto>().ReverseMap();
+                    CreateMap<Tour,UpdateTourDto>().ReverseMap();
         }
     }
 }
---
+++ Project3Travelin/ViewComponents/TourViewComponents/_TourHeadComponentsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project3Travelin.ViewComponents.TourViewComponents
+{
+    public class _TourHeadComponentsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
